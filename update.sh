e3f13c745d1fc5bb8b9ef8bd3cfd308590a1ec7f
CLOUDFALL <anonymouseo0264839@gmail.com>
2026-07-29 15:59:33 +0000

    Remove files senstive

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "✅ main.py sudah terbaru!"
-echo "✅ main.py updated!"
-git add main.py
-git commit -m "Update main.py"
-git pull --rebase origin main
-git push origin main
-echo "✅ Uploaded to GitHub!"
