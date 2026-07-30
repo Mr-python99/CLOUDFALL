57d3c30f559548ee1a0056627915367ff2d993f7
spectra engine <anonymouseo0264839@gmail.com>
2026-07-30 16:40:31 +0000

    Remove sensitive files

--- modules/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp dork_db_backup.py dork_db.py
-cp animations_backup.py animations.py
-echo "✅ Modules updated!"
-git add dork_db.py animations.py
-git commit -m "Update modules"
-git pull --rebase origin main
-git push origin main
-echo "✅ Uploaded to GitHub!"
